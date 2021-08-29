4367c09549f30c42426dfdf430f5f9dc1d16d0d4
EC2 Default User <ec2-user@ip-172-31-22-11.us-east-2.compute.internal>
2021-08-30 01:41:42 +0900

    投稿のバリデーション

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -15,10 +15,12 @@
         <form action="/posts" method="POST">
             {{ csrf_field() }}
             <div class="title">
-                <input type="text" name="post[title]" placeholder="タイトル文"/>
+                <input type="text" name="post[title]" placeholder="タイトル文" value="{{ old('post.title') }}"/>
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="body">
-                <textarea name="post[body]" placeholder="ここに画像を添付したい"/></textarea>    
+                <textarea name="post[body]" placeholder="ここに画像を添付したい"/>{{ old('post.body') }}</textarea> 
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             <input type="submit" value="投稿"/>
         </form>
